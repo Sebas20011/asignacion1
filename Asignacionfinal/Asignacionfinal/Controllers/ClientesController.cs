6f4f4061fb8690e025b5e59ebdc346c98c0290fc
Sebas20011 <naranjosebastian399@gmail.com>
2025-06-05 17:57:49 -0600

    Add files via upload

---
+++ Asignacionfinal/Asignacionfinal/Controllers/ClientesController.cs
@@ -0,0 +1,20 @@
+﻿using Asignacionfinal.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asignacionfinal.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClientesController : ControllerBase
+    {
+        private static List<Cliente> clientes = new List<Cliente>
+    {
+        new Cliente { Id = 1, Nombre = "Ana", Edad = 25 },
+        new Cliente { Id = 2, Nombre = "Luis", Edad = 30 },
+        new Cliente { Id = 3, Nombre = "Carlos", Edad = 28 }
+    };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Cliente>> Get() => clientes;
+    }
+}
